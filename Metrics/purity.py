5953fef1800ee4790bb93bb17b769c75d64bb0ce
Hanna Rudakouskaya <hannarud@mail.ru>
2018-10-18 16:41:07 +0300

    Data storage prototype changed

--- Metrics/purity.py
+++ Metrics/purity.py
@@ -9,27 +9,27 @@ import numpy as np
 from copy import deepcopy
 
 def confusion_matrix(clusters, classes_gt):
-	new_gt = deepcopy(classes_gt)
-	l = list(set(classes_gt))
-	for i in range(len(classes_gt)):
-		for j in range(len(l)):
-			if classes_gt[i] == l[j]:
-				new_gt[i] = j
-				
-	conf_mat = np.zeros([len(set(clusters)), len(set(new_gt))])
-	for i in range(len(clusters)):
-		conf_mat[clusters[i], new_gt[i]] += 1
+    new_gt = deepcopy(classes_gt)
+    l = list(set(classes_gt))
+    for i in range(len(classes_gt)):
+        for j in range(len(l)):
+            if classes_gt[i] == l[j]:
+                new_gt[i] = j
+                
+    conf_mat = np.zeros([len(set(clusters)), len(set(new_gt))])
+    for i in range(len(clusters)):
+        conf_mat[clusters[i], new_gt[i]] += 1
 
-	return conf_mat
+    return conf_mat
 
 def purity(clusters, classes_gt):
-	conf_mat = confusion_matrix(clusters, classes_gt)
-	sum_clu  = np.max(conf_mat, axis = 1)
-	sum_tot  = np.sum(sum_clu)
+    conf_mat = confusion_matrix(clusters, classes_gt)
+    sum_clu  = np.max(conf_mat, axis = 1)
+    sum_tot  = np.sum(sum_clu)
 
-	pur = sum_tot / len(clusters)
+    pur = sum_tot / len(clusters)
 
-	return pur
+    return pur
 
 ### TEST ###
 
--- Utils/Feature_extractor.py
+++ Utils/Feature_extractor.py
@@ -12,96 +12,93 @@ import keras.applications.inception_v3 as inc
 import keras.applications.xception as xce
 
 import pickle
-from glob import glob
 import os
 
+
 class Feature_extractor:
-	
-	def __init__(self, dataset, cnn_architecture, layer):
-		
-		self.dataset_path = "./Data/%s/" % dataset
-		self.dataset_im_path = self.dataset_path + "Images/"
-		self.dataset_feat_path = self.dataset_path + "Features/%s_%s" % (cnn_architecture, layer)
+    
+    def __init__(self, dataset, cnn_architecture, layer):
+        
+        self.dataset_path = os.path.join('.', 'data', dataset)
+        self.dataset_im_path = os.path.join(self.dataset_path, 'images')
 
-		ext_file = open( self.dataset_path + "extension.txt", "r")
-		self.extension = ext_file.read()
-		ext_file.close()
-		
-		self.n_files = len(glob(self.dataset_im_path + "*" + self.extension))
+        self.dataset_feat_path = os.path.join(self.dataset_path, 'features', str('%s_%s' % (cnn_architecture, layer)))
 
-		self.network_name = cnn_architecture
-		self.layer_name   = layer
-		
-		self.get_network_characteristics()
-		print("Feature extractor: %s // %s" % (self.network_name, self.layer_name))
+        self.dataset_im_names = os.listdir(self.dataset_im_path)
+        self.n_files = len(self.dataset_im_names)
 
-	def get_network_characteristics(self):
-		if self.network_name == "vgg16":
-			base_model = vgg16.VGG16(weights='imagenet')
-			self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)
-			self.tgt_size = (224, 224)
-			self.prpc_fct = prpc_vgg_res
-			
-		elif self.network_name == "vgg19":
-			base_model = vgg19.VGG19(weights='imagenet')
-			self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)        
-			self.tgt_size = (224, 224)
-			self.prpc_fct = prpc_vgg_res
-			
-		elif self.network_name == "resnet":
-			base_model = res.ResNet50(weights='imagenet')
-			self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)        
-			self.tgt_size = (299, 299)
-			self.prpc_fct = prpc_vgg_res
-			
-		elif self.network_name == "xception":
-			base_model = xce.Xception(weights='imagenet')
-			self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)        
-			self.tgt_size = (299, 299)
-			self.prpc_fct = prpc_xce_inc
-			
-		elif self.network_name == "inception":
-			base_model = inc.InceptionV3(weights='imagenet')
-			self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)
-			self.tgt_size = (299, 299)
-			self.prpc_fct = prpc_xce_inc
-			
-		else:
-			print("Error: possible network names:\n-'vgg16'\n-'vgg19'\n-'resnet'\n-'xception'\n-'inception'")
-			
-	def get_PIL_image(self, image_name):
-		file_name = self.dataset_im_path + image_name + self.extension
-		
-		return image.load_img(file_name, target_size = self.tgt_size)
-	
-	def get_arr_image(self, image_name):
-		pil_im = self.get_PIL_image(image_name)
-	
-		return image.img_to_array(pil_im)
-	
-	def get_prpc_image(self, image_name):
-		arr_im = self.get_arr_image(image_name)
-		arr_im = np.expand_dims(arr_im, axis = 0)
-		
-		return self.prpc_fct(arr_im)
-	
-	def extract(self, image_name):
-		prpc_im = self.get_prpc_image(image_name)
-		
-		return np.ndarray.flatten(self.model.predict(prpc_im))
+        self.network_name = cnn_architecture
+        self.layer_name = layer
+        
+        self.get_network_characteristics()
+        print("Feature extractor: %s // %s" % (self.network_name, self.layer_name))
 
-	def extract_and_save_features(self):
-		if not os.path.exists(self.dataset_feat_path):
-			os.makedirs(self.dataset_feat_path)
-		else:
-			return
-		
-		print("extracting and saving features ... ")
-		
-		for im in range(self.n_files):
-			if im % 100 == 0:
-				print("    %d/%d" % (im, self.n_files))
-			features = self.extract(str(im))
-			feat_file = open(self.dataset_feat_path + "/%s.p" % im, "wb")
-			pickle.dump(features, feat_file)
-			feat_file.close()
+    def get_network_characteristics(self):
+        if self.network_name == "vgg16":
+            base_model = vgg16.VGG16(weights='imagenet')
+            self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)
+            self.tgt_size = (224, 224)
+            self.prpc_fct = prpc_vgg_res
+            
+        elif self.network_name == "vgg19":
+            base_model = vgg19.VGG19(weights='imagenet')
+            self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)        
+            self.tgt_size = (224, 224)
+            self.prpc_fct = prpc_vgg_res
+            
+        elif self.network_name == "resnet":
+            base_model = res.ResNet50(weights='imagenet')
+            self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)        
+            self.tgt_size = (299, 299)
+            self.prpc_fct = prpc_vgg_res
+            
+        elif self.network_name == "xception":
+            base_model = xce.Xception(weights='imagenet')
+            self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)        
+            self.tgt_size = (299, 299)
+            self.prpc_fct = prpc_xce_inc
+            
+        elif self.network_name == "inception":
+            base_model = inc.InceptionV3(weights='imagenet')
+            self.model = Model(inputs=base_model.input, outputs=base_model.get_layer(self.layer_name).output)
+            self.tgt_size = (299, 299)
+            self.prpc_fct = prpc_xce_inc
+            
+        else:
+            print("Error: possible network names:\n-'vgg16'\n-'vgg19'\n-'resnet'\n-'xception'\n-'inception'")
+
+    def get_PIL_image(self, image_name):
+        return image.load_img(image_name, target_size = self.tgt_size)
+    
+    def get_arr_image(self, image_name):
+        pil_im = self.get_PIL_image(image_name)
+    
+        return image.img_to_array(pil_im)
+    
+    def get_prpc_image(self, image_name):
+        arr_im = self.get_arr_image(image_name)
+        arr_im = np.expand_dims(arr_im, axis=0)
+        
+        return self.prpc_fct(arr_im)
+    
+    def extract(self, image_name):
+        prpc_im = self.get_prpc_image(image_name)
+
+        return np.ndarray.flatten(self.model.predict(prpc_im))
+
+    def extract_and_save_features(self):
+        if not os.path.exists(self.dataset_feat_path):
+            os.makedirs(self.dataset_feat_path)
+        else:
+            return
+        
+        print("extracting and saving features ... ")
+        
+        for im in range(self.n_files):
+            if im % 100 == 0:
+                print("    %d/%d" % (im, self.n_files))
+            features = self.extract(os.path.join(self.dataset_im_path, self.dataset_im_names[im]))
+            features_filename = os.path.splitext(self.dataset_im_names[im])[0]
+            feat_file = open(os.path.join(self.dataset_feat_path, str("%s.p" % features_filename)), "wb")
+            pickle.dump(features, feat_file)
+            feat_file.close()
