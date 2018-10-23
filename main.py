7ee14c58052dd47a03d471b0cb296284fa0a0a16
Hanna Rudakouskaya <hannarud@mail.ru>
2018-10-23 12:10:02 +0300

    some docs added

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from Utils.Feature_extractor import Feature_extractor
 from Utils.Clusterer import Clusterer
 
 dataset = "fencing"
-cnn_architecture = "vgg19"
+cnn_architecture = "vgg19"  # Options available: 'vgg16', 'vgg19', 'resnet', 'xception', 'inception'
 layer = "fc2"
-clustering_algorithm = "agglomerative"
-metric = "all"  # Options available: "nmi", "purity", "confusion matrix", "all" (= "nmi' + "purity" + "confusion matrix")
+clustering_algorithm = "agglomerative"  # Options available: 'kmeans', 'mb_kmeans', 'affinity_prop', 'mean_shift', 'agglomerative', 'birch', 'dbscan'
+metric = "all"  # Options available: "nmi", "purity", "confusion matrix", "all" (= "nmi'+"purity"+"confusion matrix")
 
 fe = Feature_extractor(dataset, cnn_architecture, layer)
 fe.extract_and_save_features()
